448a19c580f73f817f90059f67a37c9dd51968ab
JeffersonMacanudo <jeffersontopmacapa@gmail.com>
2024-09-25 12:32:50 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-$conn = 'mysql:host=localhost;dbname=dbstalucia';
+$conn = 'mysql:host=localhost;dbname=nomedobanco';
 $user = 'root';
-$pass = 'Macanudo';
+$pass = 'senha';
 try {
     $pdo = new PDO($conn, $user, $pass, 
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));   
